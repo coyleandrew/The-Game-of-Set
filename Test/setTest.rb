0c43d1a44b5ced94d5590a5af7bcc47e7a66db08
Andrew Coyle <coyle.128@osu.edu>
2020-09-16 21:04:21 -0400

    Added test cases to setTest.rb

--- Test/setTest.rb
+++ Test/setTest.rb
@@ -1,3 +1,4 @@
+# Test file for IsASet.rb
 $LOAD_PATH << File.join(File.dirname(__FILE__), "../Model")
 $LOAD_PATH << File.join(File.dirname(__FILE__), "../lib")
 require "Card"
@@ -18,38 +19,75 @@ card5 = Card.new 1, 0, 2, 2
 card6 = Card.new 3, 0, 3, 2
 
 puts IsASet.a_set(card4, card5, card6)
-puts
-#---------------------------------------
+puts "****************************************"
 puts "Testing sets()"
 
-#Test case 1 for sets(): 
-card7 = Card.new 1, 0, 1, 2         
-card8 = Card.new 1, 0, 2, 1
-card9 = Card.new 1, 0, 3, 0
+#Test case 1 for sets(): No sets
+card7 = Card.new 2, 2, 3, 2         
+card8 = Card.new 1, 1, 3, 2
+card9 = Card.new 1, 0, 2, 1
 card10 = Card.new 2, 0, 2, 2
-card11 = Card.new 3, 0, 1, 1
+card11 = Card.new 1, 1, 1, 2
+card12 = Card.new 1, 1, 1, 1
+card13 = Card.new 2, 1, 3, 1
+card14 = Card.new 2, 0, 3, 2
+card15 = Card.new 1, 0, 2, 2        
+card16 = Card.new 1, 0, 1, 1
+card17 = Card.new 1, 0, 3, 2
+card18 = Card.new 2, 0, 3, 1
 
-=begin
-card12 = Card.new 3, 0, 2, 0
-card13 = Card.new 1, 0, 2, 2
-card14 = Card.new 1, 0, 2, 1
-card15 = Card.new 1, 0, 2, 0        
-card16 = Card.new 1, 0, 2, 2
-card17 = Card.new 1, 0, 2, 1
-card18 = Card.new 1, 0, 2, 0
-=end
-
-deck = [card7, card8, card9, card10, card11]
+deck = [card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18]
 
 sets = IsASet.sets(deck)
 
-for i in 0...sets.length()
-        print "Set #", i+1, "\n"
-        puts  "-------"
-        for j in 0...3
-                print "Card #", j+1, ":\n"
-                print sets[i][j].to_s, " "
+if sets.length > 0
+        for i in 0...sets.length()
+                print "Set #", i+1, "\n"
+                puts  "******"
+                for j in 0...3
+                        print "Card #", j+1, ":\n"
+                        print sets[i][j].to_s, " "
+                        puts
+                end
                 puts
         end
-        puts
+else
+        puts "No Sets"
 end
+
+puts "---------------------------------------"
+
+#Test case 2 for sets(): 2 sets
+card19 = Card.new 0, 1, 3, 1         
+card20 = Card.new 1, 1, 3, 2
+card21 = Card.new 0, 1, 1, 2
+card22 = Card.new 2, 0, 2, 2
+card23 = Card.new 1, 1, 1, 2
+card24 = Card.new 1, 1, 1, 1
+card25 = Card.new 2, 0, 2, 1
+card26 = Card.new 0, 0, 1, 1
+card27 = Card.new 0, 0, 2, 2        
+card28 = Card.new 0, 2, 3, 2
+card29 = Card.new 2, 2, 2, 2
+card30 = Card.new 2, 0, 3, 1
+
+deck = [card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30]
+
+sets = IsASet.sets(deck)
+
+if sets.length > 0
+        for i in 0...sets.length()
+                print "Set #", i+1, "\n"
+                puts  "-------"
+                for j in 0...3
+                        print "Card #", j+1, ":\n"
+                        print sets[i][j].to_s, " "
+                        puts
+                end
+                puts
+        end
+else
+        puts "No Sets"
+end
+
+puts "---------------------------------------"
